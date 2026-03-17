0f21de4e5dadd041862de8a1520b9d646189ecc1
medhiaghorbel-oneedge <dhiaghorbel.oneedge@gmail.com>
2026-03-17 11:18:39 +0100

    init users, reports & renderer apps

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "apps.core",
+    "apps.users",
+    "apps.reports",
+    "apps.renderer",
 ]
 
 REST_FRAMEWORK = {
